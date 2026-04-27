b70c3bd8768d484342e5413d8fe5b6766aeb15e6
Cobin-Black <cobintblack@gmail.com>
2026-04-27 13:33:48 -0400

    feat: Made the for-loop that generates all possible solutions
    feat: Created the getProfit() method needed for the solve() method

--- src/main/java/org/example/KnapsackBruteForce.java
+++ src/main/java/org/example/KnapsackBruteForce.java
@@ -22,10 +22,28 @@ public class KnapsackBruteForce
         int currentWeight;
         int currentProfit;
 
-        boolean[][] solutions;
+        boolean[][] solutions = new boolean[0][];
         boolean[] currentSolution;
         boolean[] maxSolution;
 
         int numSolutions = (int) Math.pow(2, numOfItems);
+
+        /*
+            This generates all the possible solutions because brute force
+                is exhaustive so it needs to know all possible paths
+        */
+        for(int i = 0; i <= numSolutions - 1; i++)
+        {
+            currentSolution = new boolean[i];
+            solutions[i] = currentSolution;
+        }
+
+        maxSolution = solutions[0];
+        maxProfit = getProfit(maxSolution);
+    }
+
+    private int getProfit(boolean[] maxSolution)
+    {
+
     }
 }
