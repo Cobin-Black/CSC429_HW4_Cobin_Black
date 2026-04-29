b346baf838469511925a7349f85f24dafada5f28
Cobin-Black <cobintblack@gmail.com>
2026-04-29 13:36:42 -0400

    feat: Created the object for the KnapsackBruteForce class in main
    feat: Created the weights, values, and cap for the problem as well

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,12 @@ package org.example;
 public class Main {
     public static void main(String[] args)
     {
+        int[] weights = {1, 2, 5, 1, 3};
+        int[] values = {20, 45, 90, 5, 30};
+        int knapSackCap = 8;
 
+        KnapsackBruteForce bruteForce = new KnapsackBruteForce(weights, values, knapSackCap);
+
+        System.out.println("Brute Force Method:");
     }
 }
