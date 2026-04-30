2f5bd71e6a30cacf7cf91fb574825eab43ce3ab4
Cobin <halfkey.studio.games@gmail.com>
2026-04-30 17:36:59 -0400

    feat: Created and implemented the method createNextGeneration() for the KnapsackGA class based off pseudocode
    feat: Fully completed the solve() method in KnapsackGA

--- src/main/java/org/example/KnapsackGA.java
+++ src/main/java/org/example/KnapsackGA.java
@@ -4,16 +4,20 @@ import java.util.Random;
 
 public class KnapsackGA
 {
+    //Spec Variables
     int[] weights;
     int[] values;
     int numOfItems;
     int knapSackCap;
 
-    boolean[][] population;
-    int[] fitness;
-
+    //Pseudocode Variables
     int popSize = 20;
     int numGens = 60;
+
+    boolean[][] population;
+    int[] fitness = new int[popSize];
+
+    int mutationRate = 25;
 
     Random randomSelect = new Random();
 
@@ -58,13 +62,14 @@ public class KnapsackGA
         return maxFitnessIndividual;
     }
 
+    //#region Helper Methods for solve()
     private void calculateFitnessProfitForAll()
     {
         for(int i = 0; i <= population.length - 1; i++)
         {
             fitness[i] = calculateFitness(population[i]);
         }
-    }
+    }//end of calculateFitnessProfitForAll()
 
     private int calculateFitness(boolean[] solution)
     {
@@ -80,11 +85,81 @@ public class KnapsackGA
             }
         }
 
+        //Sets it to 0 if it is not a feasible solution
         if(solutionWeight > knapSackCap)
         {
             solutionProfit = 0;
         }
 
         return solutionProfit;
-    }
+    }//end of calculateFitness()
+
+    private boolean[][] createNextGeneration()
+    {
+        boolean[][] nextGen = new boolean[population.length][numOfItems];
+        int nextGenCount = 0;
+
+        while(nextGenCount < nextGen.length)
+        {
+            //This is the select parent section
+            boolean[] randParent1 = population[randomSelect.nextInt(population.length)];
+            boolean[] randParent2 = population[randomSelect.nextInt(population.length)];
+
+            boolean[] offSpring1 = new boolean[numOfItems];
+            boolean[] offSpring2 = new boolean[numOfItems];
+
+            /*
+                This section is where the crossover will happen for the offspring.
+
+                The crossOverPosition will get where the split of the parents is happening
+                    so the children get half of each.
+            */
+            int crossOverPosition = numOfItems / 2;
+
+            for(int i = 0; i < numOfItems; i++)
+            {
+                if(i < crossOverPosition)
+                {
+                    offSpring1[i] = randParent1[i];
+                    offSpring2[i] = randParent2[i];
+                }
+                else
+                {
+                    offSpring1[i] = randParent2[i];
+                    offSpring2[i] = randParent1[i];
+                }
+            }
+
+            /*
+                This is the mutation section.
+
+                This will compare a random number of 100 to the mutationRate to see
+                    if a mutation will happen. If it does it will just flip a random bit
+                    of the offSpring.
+            */
+            if(randomSelect.nextInt(100) < mutationRate)
+            {
+                int mutationPosition = randomSelect.nextInt(offSpring1.length);
+                offSpring1[mutationPosition] = !offSpring1[mutationPosition];
+            }
+
+            if(randomSelect.nextInt(100) < mutationRate)
+            {
+                int mutationPosition = randomSelect.nextInt(offSpring2.length);
+                offSpring2[mutationPosition] = !offSpring2[mutationPosition];
+            }
+
+            nextGen[nextGenCount] = offSpring1;
+            nextGenCount++;
+
+            if(nextGenCount < nextGen.length)
+            {
+                nextGen[nextGenCount] = offSpring2;
+                nextGenCount++;
+            }
+        }
+
+        return nextGen;
+    }//end of createNextGeneration()
+    //#endregion
 }
