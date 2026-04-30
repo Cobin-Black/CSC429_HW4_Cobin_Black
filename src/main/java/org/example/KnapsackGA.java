361e64bd9833f28bd6bb9f94247084549411a629
Cobin <halfkey.studio.games@gmail.com>
2026-04-30 14:39:16 -0400

    feat: Set up the variables needed for based off specs for the homework

--- src/main/java/org/example/KnapsackGA.java
+++ src/main/java/org/example/KnapsackGA.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class KnapsackGA
 {
+    int[] weights;
+    int[] values;
+    int numOfItems;
+    int knapSackCap;
+
+    public KnapsackGA(int[] weights, int[] values, int knapSackCap)
+    {
+        this.weights = weights;
+        this.values = values;
+        this.knapSackCap = knapSackCap;
+        this.numOfItems = weights.length;
+    }
+
     public boolean[] solve()
     {
 
