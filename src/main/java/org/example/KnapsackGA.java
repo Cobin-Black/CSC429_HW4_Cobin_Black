7dfc0ea93b772d932db67204b62784600e8c42a6
Cobin <halfkey.studio.games@gmail.com>
2026-04-30 15:45:56 -0400

    feat: Started implementing the solve() method in KnapsackGA class (not fully finished)
    feat: Created a few more variables needed based off pseudocode

--- src/main/java/org/example/KnapsackGA.java
+++ src/main/java/org/example/KnapsackGA.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import java.util.Random;
+
 public class KnapsackGA
 {
     int[] weights;
     int[] values;
     int numOfItems;
     int knapSackCap;
+
+    boolean[][] population;
+    int[] fitness;
+
+    int popSize = 20;
+    int numGens = 60;
+
+    Random randomSelect = new Random();
 
     public KnapsackGA(int[] weights, int[] values, int knapSackCap)
     {
@@ -17,6 +27,26 @@ public class KnapsackGA
 
     public boolean[] solve()
     {
+        boolean[][] nextGenerationSolutions = new boolean[popSize][numOfItems];
+        boolean[] maxFitnessIndividual = new boolean[numOfItems];
 
+        population = new boolean[randomSelect.nextInt()][randomSelect.nextInt()];
+
+        for(int i = 1; i < numGens; i++)
+        {
+            calculateFitnessProfitForAll();
+            nextGenerationSolutions = createNextGeneration();
+            population = nextGenerationSolutions;
+        }
+
+        for(int i = 0; i < population.length; i++)
+        {
+            if(population[i] > maxFitnessIndividual[i])
+            {
+
+            }
+        }
+
+        return maxFitnessIndividual;
     }
 }
