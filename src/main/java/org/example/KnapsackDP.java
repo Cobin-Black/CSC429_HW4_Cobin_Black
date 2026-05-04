9792fa80b9681712899e665526efba2d566021dd
Cobin-Black <cobintblack@gmail.com>
2026-05-04 12:49:06 -0400

    feat: Created and implemented the solve() method based off the pseudocode

--- src/main/java/org/example/KnapsackDP.java
+++ src/main/java/org/example/KnapsackDP.java
@@ -15,4 +15,50 @@ public class KnapsackDP
         this.knapSackCap = knapSackCap;
         numOfItems = weights.length;
     }
+
+    public int solve()
+    {
+        int[][] dp = new int[numOfItems][weights.length];
+
+        int currentItemProfit;
+        int currentItemWeight;
+        int currentItemValue;
+        int previousItemProfit;
+
+        for(int i = 0; i < weights.length; i++)
+        {
+            dp[0][i] = 0;
+        }
+
+        for(int i = 1; i < numOfItems; i++)
+        {
+            currentItemWeight = weights[i - 1];
+            currentItemValue = values[i - 1];
+
+            for(int j = 0; j < weights.length; j++)
+            {
+                previousItemProfit = dp[i - 1][j];
+
+                if(currentItemWeight > j)
+                {
+                    dp[i][j] = previousItemProfit;
+                }
+                else
+                {
+                    currentItemProfit = currentItemValue + dp[i - 1][j - currentItemWeight];
+
+                    if(currentItemProfit > previousItemProfit)
+                    {
+                        dp[i][j] = currentItemProfit;
+                    }
+                    else
+                    {
+                        dp[i][j] = previousItemProfit;
+                    }
+                }
+            }
+        }
+
+        return dp[numOfItems - 1][weights.length];
+    }//end of solve()
 }
